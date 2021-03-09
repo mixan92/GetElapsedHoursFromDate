326fb0c7a75328dbbfc83fbed21b0e091d358ec2
scream <58901474+mixan92@users.noreply.github.com>
2021-03-09 21:51:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let now = new Date();
+let time = new Date(now.getFullYear(), now.getMonth());
+function getElapsedHoursFromDate() {
+let a = prompt("Введите дату"); // write a negative number if you want to display the date of the last month!!! SORRY
+time.setDate(a);
+return now - time;
+}
+alert((Math.round((getElapsedHoursFromDate() / 1000 / 60 / 60)*100)/100));
