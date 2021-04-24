96203fd6e0fdbb75b177bb9088e00693e8e8e420
scream <58901474+mixan92@users.noreply.github.com>
2021-04-24 20:35:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let now = new Date();
 let time = new Date(now.getFullYear(), now.getMonth());
 function getElapsedHoursFromDate() {
-let a = prompt("Введите дату"); // write a negative number if you want to display the date of the last month!!! SORRY
+let a = prompt("Введите дату"); // write a negative number if you want to display the date of the previous and so on month!!! SORRY
 time.setDate(a);
 return now - time;
 }
